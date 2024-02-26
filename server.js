973ad31fb6957994888844f1202491b2bf04f8f4
vek03 <vektromboni@gmail.com>
2024-02-26 20:40:11 -0300

    Server

--- server.js
+++ server.js
@@ -5,23 +5,31 @@ app.get("/", (req, res) => {
     res.send('Node JS')
 })
 
-app.get("/cadastrar", (req, res) => {
-    res.send('Página de Cadastro')
+app.get("/contato", (req, res) => {
+    res.send('Página de Contato')
+})
+
+app.get("/cadastrar/:item/:name/:price/:qtdd", (req, res) => {
+    res.send('Página de Cadastro' + 
+    '<br><br>Item: ' + req.params.item +
+    '<br><br>Nome: ' + req.params.name +
+    '<br><br>Preço: ' + req.params.price +
+    '<br><br>Quantidade: ' + req.params.qtdd)
 })
 
 app.get("/listar", (req, res) => {
     res.send('Página de Listar')
 })
 
-app.get("/editar", (req, res) => {
-    res.send('Página de Editar')
+app.get("/editar/:item", (req, res) => {
+    res.send('Página de Editar<br> Item: ' + req.params.item)
 })
 
-app.post("/editar", (req, res) => {
+app.post("/editar:item", (req, res) => {
     res.send('Editando...')
 })
 
-app.post("/deletar", (req, res) => {
+app.post("/deletar:item", (req, res) => {
     res.send('Deletando...')
 })
 
